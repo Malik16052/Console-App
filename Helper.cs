2fc08fc8339466dffdb711b6f62bd0e11557874e
Malik16052 <malikea-bpa202@code.edu.az>
2025-11-12 03:57:11 +0400

    Exceptionlarin qurulmasi

---
+++ Helper.cs
@@ -0,0 +1,19 @@
+﻿namespace a;
+
+public class Helper
+{
+    public class StudentNotFoundException : Exception
+    {
+        public override string Message => "Bele bir telebe tapilmadi";
+    }
+
+    public class ClassroomNotFoundException : Exception
+    {
+        public override string Message => "Sinif tapilmadi";
+    }
+
+    public class InvalidName : Exception
+    {
+
+    }
+}
